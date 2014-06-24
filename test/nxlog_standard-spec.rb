b2015354d2e0927476dc907f558419eeaa76ad6f
David Laing <david@davidlaing.com>
2014-06-24 09:34:14 +0100

    Test that the actual log event is parsed according to the internal [NXLOG@14506 type=

--- test/nxlog_standard-spec.rb
+++ test/nxlog_standard-spec.rb
@@ -33,6 +33,58 @@ describe LogStash::Filters::Grok do
         "service" => "TradingAPI_IIS"
       }
     end
+  end
+
+  describe "The actual log event is parsed according to the internal [NXLOG@14506 type=nginx_combined]" do
+
+    config <<-CONFIG
+      filter {
+        #{File.read("target/10-syslog_standard.conf")}
+        #{File.read("target/20-nxlog_standard.conf")}
+        #{File.read("target/75-nginx_combined.conf")}
+      }
+    CONFIG
+
+    actual_nginx_event = '192.0.2.15 - - [06/Jun/2013:07:28:33 +0000] "GET /favicon.ico HTTP/1.1" 200 0 "-" "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.94 Safari/537.36"'
+    nxlog_wrapped_event = '<13>1 2014-06-23T09:54:13.275897+01:00 SHIPPER-HOSTNAME - - - ' \
+                        + '[NXLOG@14506 EventReceivedTime="2014-06-23 09:54:13" SourceModuleName="in1" SourceModuleType="im_file" path="\\\\SOURCE-HOSTNAME\\Logs\\IIS\\W3SVC1\\u_ex140623.log" host="SOURCE-HOSTNAME" service="nginx" type="nginx_combined"] ' \
+                        + actual_nginx_event
+    
+    sample("@type" => "syslog", "@message" => nxlog_wrapped_event) do
+      insist { subject["@message"] } == actual_nginx_event
+    end
+
+    # @type should be the type of the embedded event, not the nxlog/syslog "wrapper"
+    sample("@type" => "syslog", "@message" => nxlog_wrapped_event) do
+      insist { subject["@type"] } == "nginx_combined"
+    end
+
+    # @timestamp should be the timestamp of the embedded log event, not the nxlog/syslog "wrapper"
+    sample("@type" => "syslog", "@message" => nxlog_wrapped_event) do
+      insist { subject["@timestamp"] } == Time.iso8601("2013-06-06T07:28:33.000Z")
+    end
+
+    # @source info should come from [NXLOG@14506 ... ]
+    sample("@type" => "syslog", "@message" => nxlog_wrapped_event) do
+      insist { subject['@source']['path'] } == "\\\\SOURCE-HOSTNAME\\Logs\\IIS\\W3SVC1\\u_ex140623.log"
+      insist { subject['@source']['host'] } == "SOURCE-HOSTNAME"
+      insist { subject['@source']['service'] } == "nginx"
+    end
+
+    # And the actual event log should be parsed correctly
+    sample("@type" => "syslog", "@message" => nxlog_wrapped_event) do
+      insist { subject["tags"] } == [ 'syslog_standard', 'nginx' ]
+      insist { subject["@timestamp"] } == Time.iso8601("2013-06-06T07:28:33.000Z")
+      insist { subject['remote_addr'] } == '192.0.2.15'
+      insist { subject['remote_user'] } == '-'
+      insist { subject['request_method'] } == 'GET'
+      insist { subject['request_uri'] } == '/favicon.ico'
+      insist { subject['request_httpversion'] } == '1.1'
+      insist { subject['status'] } == '200'
+      insist { subject['body_bytes_sent'] } == '0'
+      insist { subject['http_referer'] }.nil?
+      insist { subject['http_user_agent'] } == '"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.94 Safari/537.36"'
+    end
 
   end
 
