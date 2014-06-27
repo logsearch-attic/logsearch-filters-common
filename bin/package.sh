f4f4d8458fed02ddb241798ccd71e10227a6aa55
David Laing <david@davidlaing.com>
2014-06-27 14:58:18 +0100

    Add bin/package.sh

---
+++ bin/package.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+basedir=$(cd `dirname $0`/..; pwd)
+BUILD_NUMBER=${BUILD_NUMBER:-dev}
+TGZ="logstash-filters-common-${BUILD_NUMBER}.tgz"
+
+echo "====> Packaging..."
+echo $(date)
+	pushd $basedir/target > /dev/null
+	tar cvzf ../$TGZ .
+	popd > /dev/null
+
+echo "=====> Done"
+echo "Filters in file: $TGZ"
