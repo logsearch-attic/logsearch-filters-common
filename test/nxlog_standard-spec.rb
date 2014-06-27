d3eaa2638a32bfe2580d97de6f0640e1250c254a
David Laing <david@davidlaing.com>
2014-06-27 14:52:09 +0100

    Ensure #13 is no longer a problem.

--- test/nxlog_standard-spec.rb
+++ test/nxlog_standard-spec.rb
@@ -88,4 +88,26 @@ describe LogStash::Filters::Grok do
 
   end
 
+
+  describe "NXLOG parser doesn't gobble part of the message" do
+
+    config <<-CONFIG
+      filter {
+        #{File.read("target/10-syslog_standard.conf")}
+        #{File.read("target/20-nxlog_standard.conf")}
+      }
+    CONFIG
+
+    nxlog_syslog_prefix = '<13>1 2014-06-23T09:54:13.275897+01:00 SHIPPER-HOSTNAME - - - ' \
+                        + '[NXLOG@14506 EventReceivedTime="2014-06-23 09:54:13" SourceModuleName="in1" SourceModuleType="im_file" path="\\\\SOURCE-HOSTNAME\\Logs\\IIS\\W3SVC1\\u_ex140623.log" host="SOURCE-HOSTNAME" service="SOURCE_SERVICE" type="SOURCE_HOST"]'
+
+    sample("@type" => "syslog", "@message" => "#{nxlog_syslog_prefix} 14:17:23.012 [RingBufferThread - default.priceMarket.impl.MTMarketPricing:0] INFO  S.C.Micros.PostThrottlePublication - (tupleid=0,count=3,avg=795,max=1838)") do
+      insist { subject['@message'] } == "14:17:23.012 [RingBufferThread - default.priceMarket.impl.MTMarketPricing:0] INFO  S.C.Micros.PostThrottlePublication - (tupleid=0,count=3,avg=795,max=1838)"  
+    end
+
+    sample("@type" => "syslog", "@message" => "#{nxlog_syslog_prefix} INFO  2014-06-26 14:12:09,582 34 ObjectPooling.Pool`1+IItemStore[[ActiveMQPubSub.ActiveMQConnectionPool.IActiveMQPooledConnection, ActiveMQPubSub, Version=1.49.0.0, Culture=neutral, PublicKeyToken=null]] Number of pooled objects in use 1") do
+      insist { subject['@message'] } == "INFO  2014-06-26 14:12:09,582 34 ObjectPooling.Pool`1+IItemStore[[ActiveMQPubSub.ActiveMQConnectionPool.IActiveMQPooledConnection, ActiveMQPubSub, Version=1.49.0.0, Culture=neutral, PublicKeyToken=null]] Number of pooled objects in use 1"  
+    end
+  end
+
 end
