714fdbd81ea30c29716be280caf9e5b08ffb3a63
Danny Berger <dpb587@gmail.com>
2014-07-09 09:28:15 -0600

    Add defaults with prioritized filters like before.
     * issue logsearch/logsearch-boshrelease#58

--- bin/build.sh
+++ bin/build.sh
@@ -8,15 +8,37 @@
 # the ERB files get processed.
 #
 
-rm -fr target/*
-mkdir -p target/
+set -e
 
+# args: [source-dir [target-dir]]
+SOURCE_DIR="${1:-src}"
+TARGET_DIR="${2:-target}"
+
+rm -fr $TARGET_DIR/*
+mkdir -p $TARGET_DIR/
+
+
+#
 # plain configs
-cp src/*.conf target/
+#
 
+for SOURCE_FILE in $(find "$SOURCE_DIR" -name *.conf) ; do
+    TARGET_NAME=`basename "$SOURCE_FILE"`
+
+    echo -n "$TARGET_NAME..."
+
+    cp "$SOURCE_FILE" "$TARGET_DIR/$TARGET_NAME"
+
+    echo "done"
+done
+
+
+#
 # erb-based configs
+#
+
 LS_HEAP_SIZE="${LS_HEAP_SIZE:=500m}"
-JAVA_OPTS="$JAVA_OPTS -XX:+TieredCompilation -XX:TieredStopAtLevel=1"
+JAVA_OPTS="${JAVA_OPTS:-} -XX:+TieredCompilation -XX:TieredStopAtLevel=1"
 
 pushd vendor/logstash > /dev/null
 . bin/logstash.lib.sh
@@ -25,9 +47,14 @@ popd > /dev/null
 basedir=$PWD
 setup
 
-for CONFERB in $(find src -name *.conf.erb) ; do
-    echo "compiling ${CONFERB}..."
-	target_file=`basename $CONFERB`
-	target_file=${target_file%.erb}
-    $RUBYCMD -e "require 'erb'; puts ERB.new(File.read('$CONFERB')).result(binding)" > target/$target_file
+for SOURCE_FILE in $(find "$SOURCE_DIR" -name *.conf.erb) ; do
+    TARGET_NAME=`basename "$SOURCE_FILE"`
+
+    echo -n "${TARGET_NAME}..."
+
+	TARGET_NAME=${TARGET_NAME%.erb}
+
+    $RUBYCMD -e "require 'erb'; puts ERB.new(File.read('$SOURCE_FILE')).result(binding)" > $TARGET_DIR/$TARGET_NAME
+
+    echo "done"
 done
--- bin/package.sh
+++ bin/package.sh
@@ -1,13 +1,20 @@
 #!/bin/bash -e
-basedir=$(cd `dirname $0`/..; pwd)
-BUILD_NUMBER=${BUILD_NUMBER:-dev}
+
+# args: [source-dir]
+SOURCE_DIR="${SOURCE_DIR:-}"
+
+if [[ "" == "$SOURCE_DIR" ]] ; then
+    SOURCE_DIR="$(cd `dirname $0`/.. ; pwd)/target"
+fi
+
+BUILD_NUMBER="${BUILD_NUMBER:-dev}"
 TGZ="logsearch-filters-common-${BUILD_NUMBER}.tgz"
 
 echo "====> Packaging..."
-echo $(date)
-	pushd $basedir/target > /dev/null
-	tar cvzf ../$TGZ .
-	popd > /dev/null
+date
+
+cd $SOURCE_DIR
+tar cvzf ../$TGZ .
 
 echo "=====> Done"
 echo "Filters in file: $TGZ"
