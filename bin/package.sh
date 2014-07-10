feb5a8f4e492b75a1e5b3892f07096ff8969c8c5
David Laing <david@davidlaing.com>
2014-07-10 08:01:24 +0100

    Include (short) GIT_HASH in package name - logsearch-filters-common-${BUILD_NUMBER}.${GIT_HASH}.tgz

--- bin/package.sh
+++ bin/package.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -e
+BASE_DIR=$(cd `dirname $0`/.. ; pwd)
 
 # args: [source-dir]
 SOURCE_DIR="${SOURCE_DIR:-}"
 
 if [[ "" == "$SOURCE_DIR" ]] ; then
-    SOURCE_DIR="$(cd `dirname $0`/.. ; pwd)/target"
+    SOURCE_DIR="$BASE_DIR/target"
 fi
 
 BUILD_NUMBER="${BUILD_NUMBER:-dev}"
-TGZ="logsearch-filters-common-${BUILD_NUMBER}.tgz"
+GIT_HASH=$(cd $BASE_DIR; git rev-list -1 HEAD . | cut -c-7)
+TGZ="logsearch-filters-common-${BUILD_NUMBER}.${GIT_HASH}.tgz"
 
 echo "====> Packaging..."
 date
